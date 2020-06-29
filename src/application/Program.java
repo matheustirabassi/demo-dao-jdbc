ed57f4eea3b4d030ed5f39e074baa97eb7a1d411
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2020-06-29 16:33:08 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -24,6 +25,11 @@ public class Program {
 		System.out.println("\n === TEST 3: seller findAll ===");
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
+		System.out.println("\n === TEST 4: seller Insert ===");
+		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new id = " + newSeller.getId());
+		
 	}
 
 }
